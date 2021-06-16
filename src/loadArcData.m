4c55f51d85d65a982f67535e557602afadbd7337
Mark <anderson.mark.az@gmail.com>
2021-06-16 13:15:53 -0600

    initial commit

---
+++ src/loadArcData.m
@@ -0,0 +1,17 @@
+% Purpose: To load the arc data from four different measurement campaigns
+%
+% Inputs:
+%   - session: Which measurement campaign were the data recorded in?
+%              Acceptable inputs are:
+%                   - 'January 2019'
+%                   - 'Spring 2019'
+%                   - 'Fall 2019'
+%                   - 'Summer 2020'
+%   - configuration: Which microphone configuration you would like to get.
+%                    Acceptable inputs are:
+
+function loadArcData(session,configuration,varargin)
+
+    
+
+end
