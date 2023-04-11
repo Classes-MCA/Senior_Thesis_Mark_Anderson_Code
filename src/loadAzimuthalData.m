c33604f5b33daeb3350aee87378f45687957742f
Mark C. Anderson <anderson.mark.az@gmail.com>
2023-04-11 06:34:30 -0600

    Fully cobra-ed

--- src/loadAzimuthalData.m
+++ src/loadAzimuthalData.m
@@ -1,22 +1,21 @@
 function azimuthalData = loadAzimuthalData(path,session,configuration,elevationAngle,azimuthalAngles)
 
-    azimuthalData = MicArray;
-
     for i = 1:length(azimuthalAngles)
         
         tempConfig = strcat(configuration," (",num2str(azimuthalAngles(i))," Degrees Azimuth)");
         
         tempConfig = convertStringsToChars(tempConfig);
         
-        data = loadArcData(path,...
-                           session,...
-                           tempConfig,...
-                           elevationAngle,...
-                           'CalculateSpectra',true);
-                       
-        azimuthalData.Microphones(i).Name = strcat(num2str(azimuthalAngles(i))," Degrees");
-        azimuthalData.Microphones(i).OTOFrequencies = data.Microphones(1).OTOFrequencies;
-        azimuthalData.Microphones(i).OTOSpectrum = data.Microphones(1).OTOSpectrum;
+        [~,data] = loadArcData(path,...
+                               session,...
+                               tempConfig,...
+                               elevationAngle,...
+                               'CalculateSpectra',true);
+
+        freqs = data.Frequencies;
+        vals = data.Values;
+        azimuthalData(i) = Spectrum(freqs,vals);
+        azimuthalData(i).Name = strcat(num2str(azimuthalAngles(i))," Degrees");
         
     end
 
--- src/performAzimuthalRotation.m
+++ src/performAzimuthalRotation.m
@@ -2,20 +2,25 @@ function comparison = performAzimuthalRotation(path,session,configuration,elevat
 
     data = loadAzimuthalData(path,session,configuration,elevationAngle,azimuthalAngles);
     
-    comparison = MicArray;
+%     comparison = MicArray;
     
     for i = 1:length(azimuthalAngles)
         
-        comparison.Microphones(i).Name = data.Microphones(i).Name;
-        comparison.Microphones(i).OTOFrequencies = data.Microphones(i).OTOFrequencies;
-        
-        subtraction = data.Microphones(i).OTOSpectrum - data.Microphones(1).OTOSpectrum; % re azimuth = 0
-        
-        comparison.Microphones(i).OTOSpectrum = subtraction;
+%         comparison.Microphones(i).Name = data.Microphones(i).Name;
+%         comparison.Microphones(i).OTOFrequencies = data.Microphones(i).OTOFrequencies;
+%         
+%         subtraction = data.Microphones(i).OTOSpectrum - data.Microphones(1).OTOSpectrum; % re azimuth = 0
+%         
+%         comparison.Microphones(i).OTOSpectrum = subtraction;
+
+        freqs = data(i).Frequencies;
+        difference = data(i).Values - data(1).Values;
+        comparison(i) = Spectrum(freqs,difference);
+        comparison(i).Name = data(i).Name;
         
     end
     
     % Putting the angles in reverse order so they plot properly
-    comparison.Microphones(:) = flip(comparison.Microphones);
+    comparison = flip(comparison);
 
 end
