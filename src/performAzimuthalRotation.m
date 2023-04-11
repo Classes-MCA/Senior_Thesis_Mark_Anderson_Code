7b54a04987a03f12e88e535880742089ab3d9fa7
Mark C. Anderson <anderson.mark.az@gmail.com>
2023-04-11 06:35:05 -0600

    Deleted old comments

--- src/performAzimuthalRotation.m
+++ src/performAzimuthalRotation.m
@@ -2,16 +2,7 @@ function comparison = performAzimuthalRotation(path,session,configuration,elevat
 
     data = loadAzimuthalData(path,session,configuration,elevationAngle,azimuthalAngles);
     
-%     comparison = MicArray;
-    
     for i = 1:length(azimuthalAngles)
-        
-%         comparison.Microphones(i).Name = data.Microphones(i).Name;
-%         comparison.Microphones(i).OTOFrequencies = data.Microphones(i).OTOFrequencies;
-%         
-%         subtraction = data.Microphones(i).OTOSpectrum - data.Microphones(1).OTOSpectrum; % re azimuth = 0
-%         
-%         comparison.Microphones(i).OTOSpectrum = subtraction;
 
         freqs = data(i).Frequencies;
         difference = data(i).Values - data(1).Values;
